14a5a05d44c4b484fe940816c784d5dae5fbf166
gameplan1975 <gameplan1975@gmail.com>
2020-05-07 10:26:29 +0000

    別ページで検索機能実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,11 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :content, presence: true
 
+  def Task.search(search, user_or_task)
+    if user_or_task == "2"
+      Task.where(['name LIKE ?', "%#{search}%"])
+    else
+      Task.all
+    end
+  end
 end
