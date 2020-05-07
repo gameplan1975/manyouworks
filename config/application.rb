56f848957ef783216b2450993d41a09311b50783
gameplan1975 <gameplan1975@gmail.com>
2020-05-07 03:16:26 +0000

    rspec環境の再構築

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,9 @@ Bundler.require(*Rails.groups)
 module Manyowork
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
