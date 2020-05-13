7a45e4bc85b2dadf881463bab348fa9aee99fadf
gameplan1975 <gameplan1975@gmail.com>
2020-05-13 07:45:58 +0000

    日付入力のテストコード実装

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -5,40 +5,15 @@ RSpec.describe 'Tasks', type: :system do
       visit new_task_path
       fill_in "Name", with: "test01"
       fill_in "Content", with: "content01"
+      fill_in "Task limit", with: "05-05-2020"
       click_button "登録する"
     end
   
     context "タスクを作成した場合" do
       it "投稿した内容が表示されること" do
-        expect(page).to have_content "test01"
+       expect(page).to have_content "test01"
         expect(page).to have_content "content01"
-      end
-    end
-
-    context '必要項目を入力して、createボタンを押した場合' do
-      it 'データが保存される' do
-       expect(page).to have_content "Task was successfully created."
-      end   
-    end
-
-    context '任意のタスク詳細画面に遷移した場合' do
-      it '該当タスクの内容が表示されたページに遷移する' do
-        visit current_path
-        expect(page).to have_content "test01"
-      end
-    end
-    context '複数のタスクを作成した場合' do
-      it '後から作成したタスクが先に表示される' do
-        click_link "Back"
-        click_link "New Task"
-        fill_in "Name", with: "test02"
-        fill_in "Content", with: "content02"
-        click_button "登録する"
-        click_link "Back"
-        expect(first("tbody tr")).to have_content "test02"
-        expect(first("tbody tr")).to have_content "content02"
-        expect(all("tbody tr")[1]).to have_content "test01"
-        expect(all("tbody tr")[1]).to have_content "content01"
+        expect(page).to have_content "2020-05-05"
       end
     end
   end
